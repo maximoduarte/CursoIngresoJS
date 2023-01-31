be78cff091fd528717d5feedd90f54db7f67264c
maximoduarte <maximoduarteprogra@gmail.com>
2023-01-31 09:46:06 -0300

    modifique ejercicio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
-/*
+/* maximo duarte
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("esto funciona de maravilla");
 }
 
