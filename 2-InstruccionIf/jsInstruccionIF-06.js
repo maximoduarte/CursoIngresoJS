75bff417d2e5aa0995d330b533782cb7d47b3437
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-07 15:57:49 -0300

    if6

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,29 @@
+//maximo duarte
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+	let mensaje;
+	edad= document.getElementById("txtIdEdad").value;
+	edad= parseInt(edad);
 
+	if(edad >17){
 
+    mensaje="es mayor de edad";
 
-}//FIN DE LA FUNCIÓN
+	
+	}
+	 else{
+        if(edad >= 13 && edad <= 17)
+	    {
+	      mensaje= "es adolescente";
+		}else
+		{
+	      mensaje=" es menor de edad";
+		}
+  	}	 
+	
+    alert(mensaje);
+}
+
+//FIN DE LA FUNCIÓN
