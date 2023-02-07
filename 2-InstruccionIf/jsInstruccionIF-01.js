c361cedba8415c343868ce609651bdd0e90f813e
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-07 09:44:56 -0300

    if 1

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,34 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+     
+	edad=document.getElementById("txtIdEdad").value;
+	edad= parseInt(edad);
+
+	if(edad==15) {
+
+
+    alert("ñiña bonita")
+
+	}
 
 }//FIN DE LA FUNCIÓN
+
+/*
+	if(condicion)//verdadero o falso (0) true -> verdadero false -> falso
+	{
+		codigo...;
+	}else
+	{
+		codigo...;		
+	}
+
+	operadores aritmeticos:
+	+ - * / %
+	operadores relacionales o condicionales
+	< > <= >= == !=
+	operadores logicos
+    condicionUno	&&  condicionDos            ||               !
+	y logica                                o logica         negacion
+	*/
