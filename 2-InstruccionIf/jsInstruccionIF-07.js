4d3fd93d6b29023c31b3b64c6ee069091a01b1da
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-08 09:32:31 -0300

    if7

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,26 @@
+//maximo duarte
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+    let edad;
+	let estadoCivil;
+	let mensaje;
+	 
+	edad=document.getElementById("txtIdEdad").value;
+	edad=parseInt(edad);
+    
+	estadoCivil=document.getElementById("estadoCivil").value;
+
+    if(edad< 18 && estadoCivil !="Soltero"){
+       
+		mensaje="es muy chiquito para no ser soltero"
+
+	}
+
+
 	
+
+     alert(mensaje)
 
 
 }//FIN DE LA FUNCIÓN
