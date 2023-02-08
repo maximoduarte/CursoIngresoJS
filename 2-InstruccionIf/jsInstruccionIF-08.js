4502fc89d19e82e2a0fea8e97f73506788baac0d
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-08 09:34:00 -0300

    if8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,18 @@
+//maximo duarte
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
 	
+	let edad;
+	let estadoCivil;
+	let mensaje;
+    
+	edad=document.getElementById("txtIdEdad").value;
+	edad=parseInt(edad);
+	estadoCivil=document.getElementById("estadoCivil").value;
 
-
+	if(!(edad< 18 || estadoCivil !="Soltero")){
+         mensaje=" es soltero y no es menor"
+	}
+       alert(mensaje);
 }//FIN DE LA FUNCIÓN
