7eb863aa119a0c362031e3d956024a5c1dafe20f
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-09 11:47:19 -0300

    switch 1

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,8 +1,32 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
+	
+	mesDelAño=document.getElementById("txtIdMes").value;
+
+	switch(mesDelAño)
+	{
+    case "Enero":
+	    mensaje="que comiences bien el año!!!.";
+		break;
+	case "Marzo" :
+		mensaje= "a clases!!!.";
+		break;
+	case "Julio":
+	    mensaje= "se vienen las clases!!!:";
+		break;
+    case "Diciembre":
+		mensaje= "felices fiestas!!!.";
+		break;
+
+	default:
+		mensaje="mes aburrido.";
+
+	}
+
+	alert(mensaje);
 
 
 
