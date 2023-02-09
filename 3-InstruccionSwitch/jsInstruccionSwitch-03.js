7cd44178aec2bcbadc8b7c2ad368f36473ae880a
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-09 11:50:07 -0300

    switch 3

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,9 +1,26 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
 	
+					
+	mesDelAño= document.getElementById("txtIdMes").value;
+
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			mensaje=" Este mes no tiene más de 29 días.";
+			break;
+
+		default:
+			mensaje="Este mes tiene 30 o más días";
+		
+ 
+
+	}
+    
+	alert(mensaje);
 	
 
 
