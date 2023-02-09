546e77e56fc9dd4b89fe72f15e28b9d95f052862
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-08 22:32:10 -0300

    if 9

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let numero;
+	let max = 10;
+	let min = 1;
+	
+	numero= Math.floor(Math.random() *10) + 1;
+    
 
+	console.log(numero);
 }//FIN DE LA FUNCIÓN
