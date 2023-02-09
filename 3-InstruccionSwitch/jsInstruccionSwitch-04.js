90899f4aa0f82e8713804bfcbd1887602374227b
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-09 11:58:45 -0300

    switch 4

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,8 +1,35 @@
+//maximo duarte
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
+
+	mesDelAño= document.getElementById("txtIdMes").value;
+
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			mensaje="tiene 28 dias";
+		break;
+
+        case"Abril":
+		case"Junio":
+		case"Septiembre":
+		case"Noviembre":
+	        mensaje="tiene 30 dias";
+        break;
+
+
+		default:
+			mensaje="tiene 31 dias";
+
+
+	}
+
+	alert(mensaje);
+
+
 	
 	
 
