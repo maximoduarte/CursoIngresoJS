d12a350145e2d60628f686cdf9e68cc118b6fe1f
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-11 20:29:38 -0300

    switch 9

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,58 @@
+//maximo duarte
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion; 
+	let destino;
+	const PRECIO= 1500;
+	let aumento = 0 ; 
+	let descuento = 0;
+	let precioFinal;   
 
+	estacion=document.getElementById("txtIdEstacion").value;
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch(estacion){
+		case "Invierno":
+			if(destino =="Bariloche"){
+			aumento= 20; 	
+			}
+            else if(destino == "Mar del plata"){
+			descuento =20;	
+			} 
+			else{
+			descuento =10;	
+			}
+        break;
+		
+		case "Verano":
+			if(destino == "bariloche"){
+			descuento= 20;	
+			}
+			else if(destino== "Mar del plata"){
+			aumento = 20;
+			}
+			else{
+			aumento=10;	
+			}
+			break;
+
+
+		case "Otoño":
+		case "Primavera":
+			if(destino != "Cordoba"){
+            aumento=10;
+			}	
+			break;
+	}
+	
+	if(aumento != 0 ){
+		precioFinal= PRECIO+ PRECIO * + aumento / 100;
+	}
+	else if(descuento != 0){
+		precioFinal=PRECIO - PRECIO * descuento /100;
+	}else{
+		precioFinal=PRECIO;
+	} 
+  
+	alert("precio final $" + precioFinal);
 }//FIN DE LA FUNCIÓN
