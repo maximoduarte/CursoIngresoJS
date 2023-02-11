885cd9b5b9936c2087fd4e3409fc9bd5377852e4
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-11 20:27:55 -0300

    switch 8

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,23 @@
+//maximo duarte
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+    let mensaje;
+    
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch(destino){
+
+    case "Bariloche":	
+	case "Usuahia":	
+		mensaje="hace FRIO";
+		break;
+
+    default:
+		mensaje="hace CALOR";
+
+	}
+     
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
