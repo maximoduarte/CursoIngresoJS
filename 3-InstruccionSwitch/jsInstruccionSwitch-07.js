54b164f30bb987c08b7a92c663eaf35bf93d36ef
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-11 20:26:17 -0300

    switch 7

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,32 @@
+//maximo duarte
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino; 
+	let mensaje;
+	
+
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch(destino){
+		case "Bariloche":
+			mensaje="se encuentra en el sur de Argentina."; 
+			break;
+		
+		case "Cataratas":		
+			mensaje="se encuentra en el norte de Argentina";
+			break;
+        
+		case "Mar del plata":
+			mensaje="se encuentra en el este de Argentina";
+			break;
+
+		default:
+			mensaje="se encuentra en el oeste de Argentina";
+			
+	}
+      alert(mensaje);
+
+
+
 
 }//FIN DE LA FUNCIÓN
