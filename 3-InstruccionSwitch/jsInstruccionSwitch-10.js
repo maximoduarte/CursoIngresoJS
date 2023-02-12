424942f7ca3d6cdbc1b3c4e7c1a8493871802bd0
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-12 01:56:17 -0300

    switch 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,53 @@
+//maximo duarte
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion;
+	let destino;
+
+	estacion=document.getElementById("txtIdEstacion").value;
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch(estacion){
+		case "Invierno":
+			if(destino == "Bariloche"){
+				mensaje="se viaja";
+			}
+			else{
+				mensaje="no se viaja"
+			}
+			break;
+		
+		case "Verano":
+			if(destino == "Mar del plata"){
+				mensaje="se viaja";	
+			}
+			else if(destino == "Cataratas"){
+				mensaje="se viaja";
+			}
+			else{
+				mensaje="no se viaja";
+			}
+			break;
+
+		case "Otoño":
+			case "Mar del plata":	
+			case "Bariloche":	
+			case "Cataratas":	
+			case "Cordobo":	
+	
+				mensaje="se viaja";
+			break;
+		
+		case "Primavera":
+			if(destino != "Bariloche"){
+				mensaje="se viaja";
+			}
+			else{
+				mensaje="no se viaja";
+			}
+			break;	
+		
+	}
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
