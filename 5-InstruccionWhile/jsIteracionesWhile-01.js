010f080ce50b9c7ded9d823f5762e48fe779baf3
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-14 23:03:52 -0300

    while 1

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,7 +1,14 @@
-/*
+/*maximo duarte
 al presionar el botón mostrar 10 repeticiones 
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
-{
-	alert('iteración while');
+{	
+let i =0;
+
+while(i < 10){
+
+i= i + 1;
+console.log(i);
+}
+
 }//FIN DE LA FUNCIÓN
