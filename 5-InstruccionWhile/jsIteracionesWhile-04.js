b530dff82f02f16548f5aeb68dd6d3383e83cb3a
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-14 23:05:00 -0300

    while 4

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,15 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
+	let numero;
+
+	numero=parseInt(numero);
+	numero=prompt("ingrese un numero del 0 al 9.");
+
+	while(numero <0 || numero > 9){
+		numero=prompt("Error:ingrese un numero entre el 0 y el 9");
+		numero=parseInt(numero);
+		
+	}
+	document.getElementById("txtIdNumero").value = numero;	
 }//FIN DE LA FUNCIÓN
