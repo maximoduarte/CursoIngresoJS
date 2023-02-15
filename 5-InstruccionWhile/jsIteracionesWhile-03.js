3f2bae15d4109ae5070a4fb9be0bb52c28acf7c9
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-14 23:04:46 -0300

    while 3

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,12 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	let clave;
+	clave = prompt("ingrese el número clave.");
+
+	while(clave !="utn750"){
+		clave=prompt("clave invalida, ingrese de nuevo su clave");
+	}
+	alert("clave valida!!!");
 	
 }//FIN DE LA FUNCIÓN
