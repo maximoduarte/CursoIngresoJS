6d4b1f0bb693a64752f594df4864e75295f52610
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-22 16:50:29 -0300

    while 7

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -2,17 +2,39 @@
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
+//maximo duarte
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+	let numero;
+	let acumulador;
+	let contador;
+	let respuesta;
+	let promedio;
+
+	contador= 0;
+	acumulador= 0;
+	respuesta= "si";
+
+	while(respuesta== "si"){
+		numero= parseInt(prompt("ingresar un numero"));
+	
+	while(isNaN(numero)){
+		numero=parseInt(prompt("error ingrese un numero"));
+	
+	}
+	respuesta = prompt("¿desea ingresar otro numero? si/no");
+
+	acumulador= acumulador +numero;
+	contador= contador + 1;
+	}
+
+	promedio= acumulador/contador;
+
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
 
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+
+	
 
 }//FIN DE LA FUNCIÓN
