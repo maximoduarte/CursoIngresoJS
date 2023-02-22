0d355d60c3e600de8241160906240daa5900605c
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-22 16:53:43 -0300

    while 9

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,42 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
+//maximo duarte
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
+	let banderaDelPrimero;
+	let numeroIngresado;
+	let numeroMaximo;
+	let numeroMinimo;
+	let respuesta;
+	//iniciar variables  
+	banderaDelPrimero= true;
 	respuesta='si';
+
 	while(respuesta=="si")
 	{
+		numeroIngresado = prompt("ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
+		while(isNaN(numeroIngresado))
+		{	
+			numeroIngresado= prompt("error, ingrese un numero");
+			numeroIngresado = parseInt(numeroIngresado);
 		
-		respuesta=prompt("desea continuar?");
-	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+		}
+		if(banderaDelPrimero == true) 
+		{						10
+			numeroMaximo= numeroIngresado;
+			numeroMinimo= numeroIngresado;
+			banderaDelPrimero = false; // para que no vuleva a entrar
+		}else if(numeroMaximo < numeroIngresado) 
+		{						9
+			numeroMaximo = numeroIngresado;
+		}else if(numeroMinimo > numeroIngresado)
+		{
+			numeroMinimo = numeroIngresado;
+		}
+			respuesta= prompt("quieres seguir? si/ no")
+ 	}	
+	 document.getElementById("txtIdMaximo").value =numeroMaximo;
+	 document.getElementById("txtIdMinimo").value =numeroMinimo;
 }//FIN DE LA FUNCIÓN
