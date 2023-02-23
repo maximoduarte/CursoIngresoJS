a5c9574c7a06b821ace09c71f5871808617413d6
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-23 15:55:45 -0300

    while 8

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,48 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
+//maximo duarte
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+    let respuesta;
+    let sumaPositivos; 
+    let multiplicadorNegativos;
+    let downflag ;
+    let numeroIngresado;
+    
+    respuesta = "si";
+    multiplicadorNegativos =1;
+	downflag =true;
+    sumaPositivos =0;
 
-
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+    while(respuesta=="si")
+	{
+		numeroIngresado = prompt("ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
+		while(isNaN(numeroIngresado))
+	    {	
+			numeroIngresado= prompt("error, ingrese un numero");
+			numeroIngresado = parseInt(numeroIngresado);
+		
+	    }
+		    if(numeroIngresado >= 0)
+             {
+            sumaPositivos += numeroIngresado;
+             }
+        else
+        {
+            if(downflag== true){
+                multiplicadorNegativos = numeroIngresado;
+                downflag= false;
+        }else{
+        
+            multiplicadorNegativos*= numeroIngresado;
+        }    
+        }      
+     respuesta= prompt( "quieres seguir? si/no");
+        
+    }    	
+   document.getElementById("txtIdSuma").value =sumaPositivos;
+   document.getElementById("txtIdProducto").value =multiplicadorNegativos;
 
 }//FIN DE LA FUNCIÓN
