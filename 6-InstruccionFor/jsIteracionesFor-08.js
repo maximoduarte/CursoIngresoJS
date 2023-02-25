24a8bd25232b825f1a1ab6d123717db3d0aec26b
maximoduarte <maximoduarteprogra@gmail.com>
2023-02-25 18:05:18 -0300

    for 8

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,29 @@
+//maximo duarte
 function mostrar()
 {
+	let numero;
+	let contadorDivisores = 0;
+	
 
 
-	alert("ok");
+	numero= parseInt(prompt("ingrese un numero"));
 
-}//FIN DE LA FUNCIÓN
+
+	for(let i= 2 ; i< numero ; i++)
+	{
+		if(numero % i == 0)
+		{
+			contadorDivisores++;
+			
+		}
+	}
+		if (contadorDivisores != 0 || numero == 1)
+		{
+			alert(numero + " no es primo ");
+		}
+		else
+		{
+			alert(numero + " es primo ");
+		}
+	
+}
